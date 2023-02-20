e62b75001200879cbd223b8ca850c5b3eac4d87a
lazypanda31 <83162435+lazypanda31@users.noreply.github.com>
2023-02-20 19:22:53 +0530

    add title

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,24 @@ import '@/styles/globals.css'
 import LayoutWrapper from "@/components/LayoutWrapper";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  let routeName = capitalizeFirstLetter(router.pathname.substr(1));
+  if (routeName.length === 0) {
+    routeName = "Home";
+  }
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <Head>
+        <title>{`${routeName} | PACE 2023`}</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <LayoutWrapper>
